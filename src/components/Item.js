ef6aee94e914dc7263ca67266f190aaaaddab57e
Dennis Kemboi <denkogei11@gmail.com>
2024-11-01 13:33:26 +0300

    react hooks state

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Item({ name, category }) {
+  const [addCart, setAddCart] = useState(false);
+  function handleAddItem(){
+    setAddCart((addItem) => !addItem)
+  }
   return (
-    <li className="">
+    <li className={addCart ? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddItem}>{addCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
